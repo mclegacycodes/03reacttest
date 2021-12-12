42989b68af937a3a178e0a60c41af9a9daabcffc
unknown <kingsleytwum2@gmail.com>
2021-12-12 12:53:07 +0000

    day 3 commit

--- src/index.js
+++ src/index.js
@@ -1,56 +1,54 @@
-import React from 'react'
-import reactDom from 'react-dom'
+import React from "react";
+import reactDom from "react-dom";
 
-
-//css
+// css
 import './index.css'
 
-function BookList(){
-    return (
-        <section className='booklist'>
-           {/* //call the section header */}
-           <SectionHead/>
-           <Books/>
-        </section>
-    )
-}
+const features = [
+    {
+        id:1,
+        image:"https://i.pinimg.com/474x/f1/9a/a3/f19aa34981b8e781b37506e31422e141.jpg",
+        title:"Audio and HD video calling",
+        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste laudantium dolor enim sit harum voluptatum distinctio earum. Repellat, similique tenetur!"
+    },
+     {
+        id:2,
+        image:"https://cdn0.iconfinder.com/data/icons/communication-network-2/70/network__connection__sharing__LCD__screen-512.png",
+        title:"Screen sharing",
+        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste laudantium dolor enim sit harum voluptatum distinctio earum. Repellat, similique tenetur!"
+    },
+    {
+        id:3,
+        image:"https://cdn3.iconfinder.com/data/icons/devices-set-vol-2/512/Device_and_Media_icons-76-512.png",
+        title:"Smart messaging",
+        description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste laudantium dolor enim sit harum voluptatum distinctio earum. Repellat, similique tenetur!"
+    }
+]
 
-
-
-
-
-const SectionHead=()=>{
-    return (
-    <div className='sectionHead'>
-        <h3 className='h2'>Inspiration for your <br />next trip</h3>
+const CheckFeatures=()=>{
+    return <section>
         <span>
-            <input type="button" value="<" />
-            <input type="button" value=">" />
+            <h1>Checkout All Features</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br />Nostrum tempora perferendis iure sed et illo laboriosam velit debitis.</p>
         </span>
-            
+        <FeatureList/>
+    </section>
+}
+const FeatureList=()=>{
+    return <section className="featurelist">
+{features.map((item)=>{
+    return <Feature key={item.id} features={item}/>
+    })}
+    </section>
+}
+
+const Feature=(props)=>{
+    const{image,title,description}=props.features
+    return <div>
+        <img src={image} alt="feature image" />
+        <h3>{title}</h3>
+        <p>{description}</p>
     </div>
-    )
 }
 
-const Books=()=>{
-return (
-    <section className='books'>
-     <Book/>
-      <Book/>
-       <Book/>
-       
-       </section>
-)
-}
-
-const Book =()=>{
-return <div className='book'>
-    <img src="https://a0.muscache.com/im/pictures/a433b4d0-8183-4523-b4c5-99b81c5729c1.jpg?im_w=320" alt="a location in capecoast" />
-    <span>
-    <h3>CapeCoast</h3>
-    <p>5 kilometers away</p>
-    </span>
-</div>
-}
-
-reactDom.render(<BookList/>, document.getElementById('root'))
+reactDom.render(<CheckFeatures/>,document.getElementById('root'))
